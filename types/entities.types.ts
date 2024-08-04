0f4cc993b3b9e8896751d08f298fcff47e15ce42
wilfreud <killlergangster@gmail.com>
2024-08-04 17:19:26 +0000

    feat: typing system

---
+++ types/entities.types.ts
@@ -0,0 +1,138 @@
+// Enum type for Acuité Visuelle Oeil
+export enum AcuiteVisuelleOeil {
+  Zero = "0",
+  ZeroPointTwentyFive = "0.25",
+  ZeroPointFive = "0.5",
+  One = "1",
+  Two = "2",
+  Three = "3",
+  Four = "4",
+  Five = "5",
+  Six = "6",
+  Seven = "7",
+  Eight = "8",
+  Nine = "9",
+  Ten = "10",
+}
+
+// Grades
+export enum Grade {
+  DR = "Dr",
+  PR = "Pr",
+}
+
+// Admin Principal
+export interface AdminPrincipal {
+  id: number;
+  nom: string;
+  prenom: string;
+  role: Grade;
+  date_creation?: Date;
+  date_modification?: Date;
+}
+
+// Medecin
+export interface Medecin {
+  id: number;
+  admin_principal_id: number;
+  role: Grade;
+  prenom: string;
+  nom: string;
+  service: string;
+  telephone: string;
+  confirmer_telephone: string;
+  date_creation?: Date;
+  date_modification?: Date;
+}
+
+// Auxiliaire
+export interface Auxiliaire {
+  id: number;
+  admin_principal_id: number;
+  prenom: string;
+  nom: string;
+  service: string;
+  code: string;
+  telephone: string;
+  confirmer_telephone: string;
+  date_creation?: Date;
+  date_modification?: Date;
+}
+
+// Sexe
+export enum Sexe {
+  M = "M",
+  F = "F",
+}
+
+// Patient
+export interface Patient {
+  id: number;
+  auxiliaire_id: number;
+  date_enregistrement: Date;
+  no_fiche: string;
+  nom: string;
+  prenom: string;
+  sexe: Sexe;
+  age: Date;
+  adresse: string;
+  profession: string;
+  telephone: string;
+  confirmer_telephone: string;
+  observation?: string;
+  pas_glaucome_reevaluation?: boolean;
+  risque_glaucome_examens?: boolean;
+  gpao?: boolean;
+  date_creation?: Date;
+  date_modification?: Date;
+}
+
+// Antecedents Personnels
+export interface AntecedentsPersonnels {
+  id: number;
+  patient_id: number;
+  HTA?: boolean;
+  diabete?: boolean;
+  drepanocytose?: boolean;
+  atopie?: boolean;
+  addiction?: boolean;
+  type_addiction?: string;
+  autres?: string;
+  pathologie_ophtalmologique?: string;
+  traitement?: string;
+}
+
+// Antecedents Familiaux
+export interface AntecedentsFamiliaux {
+  id: number;
+  patient_id: number;
+  cecite?: boolean;
+  GPAO?: boolean;
+  autres?: string;
+}
+
+// Retinographie
+export interface Retinographie {
+  id: number;
+  patient_id: number;
+  segment_anterieur_retinographie?: string;
+  fichier_joint?: string;
+}
+
+// Constantes Traitement
+export interface ConstantesTraitement {
+  id: number;
+  patient_id: number;
+  acuite_visuelle_correction?: boolean;
+  od?: AcuiteVisuelleOeil;
+  og?: AcuiteVisuelleOeil;
+  odg?: AcuiteVisuelleOeil;
+  refraction_automatisee_A?: number;
+  refraction_automatisee_S?: number;
+  refraction_automatisee_C?: number;
+  refraction_automatisee_DP?: number;
+  tonus_oculaire?: number;
+  pachymetrie?: number;
+  cd?: number;
+  traitement_hypotonisant_oculaire?: boolean;
+}
