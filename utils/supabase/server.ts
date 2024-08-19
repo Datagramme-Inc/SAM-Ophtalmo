4d0bf51414c30849b6e38a6425cd40fd109bb7ef
IbrahimaMinianeDiouf <mignzii99@gmail.com>
2024-08-19 11:58:43 +0000

    last

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,10 +1,8 @@
-'use server'
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-
 export const createClient = () => {
   const cookieStore = cookies();
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
